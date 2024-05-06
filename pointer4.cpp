562f4026e15c2e5c5f9e17046b310a2bc8137a68
Yurcell <chestayurcel@gmail.com>
2024-05-06 15:43:50 +0700

    membuat fungsi void setnama untuk mengatur nilai dari sebuah anggota data dari objek kelas mahasiswa

--- pointer4.cpp
+++ pointer4.cpp
@@ -10,3 +10,15 @@ private:
 public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string pNama)
+{
+    a.nama = pNama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
