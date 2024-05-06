829370a9fc39d29a0eb74178e4ae0bd0a67f8229
Yurcell <chestayurcel@gmail.com>
2024-05-06 15:02:53 +0700

    membuat pointer reference refMhs

--- pointer1.cpp
+++ pointer1.cpp
@@ -14,5 +14,8 @@ int main(){
     mahasiswa mhs(1); // Object mhs
     mhs.showNim(); // Member Access Operator
 
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
 
 }
